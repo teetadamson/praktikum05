0ac7b61bf000cc1f1a849b2d939e84d75546daf2
Teet Adamson <teet.adamson@itcollege.ee>
2017-11-01 12:52:56 +0200

    c terav

---
+++ Inimene.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktikum05
+{
+    class Inimene
+    {
+        public string Eesnimi;
+        public string Perenimi;
+        public int Pikkus;
+
+        public override string ToString()
+        {
+            return $"{Eesnimi} {Perenimi} {Pikkus}";
+        }
+
+ 
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Praktikum05
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Inimene> _inimesed;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _inimesed = new List<Inimene>();
+            lboxInimesed.ItemsSource = _inimesed;
+        }
+
+        private void btnLisa_Click(object sender, RoutedEventArgs e)
+        {
+            if (lboxInimesed.SelectedIndex < 0)
+            {
+                Inimene uusInimene = new Inimene();
+                uusInimene.Eesnimi = txtEesnimi.Text;
+                uusInimene.Perenimi = txtPerenimi.Text;
+                uusInimene.Pikkus = int.Parse(txtPikkus.Text);
+                _inimesed.Add(uusInimene);
+            }
+            else
+            {
+                var muudetav = lboxInimesed.SelectedItem as Inimene;
+                muudetav.Perenimi = txtPerenimi.Text;
+                muudetav.Eesnimi = txtEesnimi.Text;
+                muudetav.Pikkus = int.Parse(txtPikkus.Text);
+                btnLisa.Content = "Lisa";
+                lboxInimesed.SelectedIndex = -1;
+            }
+            lboxInimesed.Items.Refresh();
+        }
+
+
+
+
+
+
+        private void lboxInimesed_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //SelectedIndex  > -1 siis listboxist on midagi valitud
+            //SelectedIndex == -1 (listboxist ei ole midagi valitud)
+            if (lboxInimesed.SelectedIndex > -1)
+            {
+                //Küsime inimese, kes valiti
+                Inimene valitud = lboxInimesed.SelectedItem as Inimene;
+
+                if (valitud != null)
+                {
+                    txtEesnimi.Text = valitud.Eesnimi;
+                    txtPerenimi.Text = valitud.Perenimi;
+                    txtPikkus.Text = valitud.Pikkus.ToString();
+                    btnLisa.Content = "Muuda"; //muudab Lisa nupu Muuda-ks
+                }
+            }
+        }
+    }
+    } 
+
